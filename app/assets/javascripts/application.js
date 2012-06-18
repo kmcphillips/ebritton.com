44652781c11218e382846f8bd4d291f43e579803
Kevin McPhillips <github@kevinmcphillips.ca>
2012-06-17 22:28:52 -0500

    Add the lightbox plugin.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function(){
+  $("#col_right a:has(img)").fancybox({
+    openEffect: "elastic",
+    closeEffect: "elastic",
+    helpers: {
+      title : {
+        type : 'inside'
+      }
+    }
+  });
+});
