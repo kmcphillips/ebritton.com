231d957ddd724a2da700d85b9804b0fb311e1f6b
Kevin McPhillips <github@kevinmcphillips.ca>
2014-03-20 22:02:08 -0400

    Modernize routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,41 +4,40 @@ EbrittonCom::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  root :to => "blog#index"
+  root to: "blog#index"
 
-  resources :blog, :only => [:index, :show] do
+  resources :blog, only: [:index, :show] do
     collection do
       get :archive
     end
   end
 
-  match "feed.:format" => "blog#feed"
-  match "rss.xml" => "blog#feed", :format => "rss"
+  get "feed.:format" => "blog#feed"
+  get "rss.xml" => "blog#feed", :format => "rss"
 
   [:bio, :contact].each do |block|
-    match block.to_s => "blocks##{block}"
+    get block.to_s => "blocks##{block}"
   end  
 
-  match "p" => "blocks#secret"
-  match "playlist" => "blocks#playlist"
+  get "p" => "blocks#secret"
+  get "playlist" => "blocks#playlist"
 
-  resources :links, :only => [:index]
-  resources :projects, :only => [:index, :show]
-  resources :events, :only => [:index, :show]
+  resources :links, only: [:index]
+  resources :projects, only: [:index, :show]
+  resources :events, only: [:index, :show]
 
-  match "admin/preview" => "preview#index"
-
+  match "admin/preview" => "preview#index", via: [:get, :post]
 
   ## Legacy PHP URLs
   %w[blog events bio bloghistory works links contact].each do |page|
-    match "#{page}.php" => "legacy##{page}"
+    get "#{page}.php" => "legacy##{page}"
   end
 
   # Catch all remaining PHP pages and send them somewhere
-  match ":page.php" => "legacy#default"
+  get ":page.php" => "legacy#default"
 
   # Legacy file locations
-  match "/media/:filename" => "legacy#media"
+  get "/media/:filename" => "legacy#media"
 
 end
 
