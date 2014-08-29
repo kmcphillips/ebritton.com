47b8d7f7b70776af6a27f3348715191b948f2d44
Kevin McPhillips <github@kevinmcphillips.ca>
2014-08-28 21:26:53 -0400

    Add config cache

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,8 @@ EbrittonCom::Application.configure do
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
 
+  config.eager_load = false
+
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
 
