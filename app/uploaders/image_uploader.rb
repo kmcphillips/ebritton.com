db054201b77cbb94cc59e1f11f7b50a25eb72868
Kevin McPhillips <github@kevinmcphillips.ca>
2014-08-28 21:27:03 -0400

    Remove unneeded modules for carrier wave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,5 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::RMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
 
   storage :file
 
@@ -16,14 +11,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
   end
 
-  process :resize_to_fit => [700, 700]
+  process resize_to_fit: [700, 700]
 
   version :thumb do
-    process :resize_to_fit => [160, 160]
+    process resize_to_fit: [160, 160]
   end
 
   version :column do
-    process :resize_to_fit => [360, 360]
+    process resize_to_fit: [360, 360]
   end
 
   def extension_white_list
