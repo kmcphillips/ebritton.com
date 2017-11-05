e745fad63f5da55d892f8671fee55367e88b7663
Kevin McPhillips <github@kevinmcphillips.ca>
2017-11-04 22:50:01 -0400

    Add back the Let's Encrypt .well-known line for cert renewal

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ lock '3.6.1'
 set :application, 'ebritton.com'
 set :repo_url, 'https://github.com/kmcphillips/ebritton.com.git'
 
+append :linked_dirs, 'public/.well-known'
+
 set :user, "deploy"
 set :rbenv_ruby, "2.3.1"
 
